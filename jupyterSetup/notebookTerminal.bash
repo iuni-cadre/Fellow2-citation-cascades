bc746a69f3b19c96c8bb89d7ed9ef8ce035c9723
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2019-08-13 16:07:33 -0400

    Create notebookTerminal.bash

---
+++ jupyterSetup/notebookTerminal.bash
@@ -0,0 +1,17 @@
+conda info --envs
+conda create -y -p /home/jovyan/conda/dbconnect python=3.5 
+source /opt/conda/etc/profile.d/conda.sh
+conda activate /home/jovyan/conda/dbconnect
+
+#conda create --name test python=3.5
+#conda init
+
+--------------restart terminal----------------
+
+pip uninstall pyspark
+pip install -U databricks-connect==5.3.*
+conda install -c anaconda openjdk (version 8.0.152)
+databricks-connect test
+
+conda install nb_conda
+python -m ipykernel install --user --name databricks --display-name "Python (databricks-backend)"
