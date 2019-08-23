145dcc2cb85d7399d17238f94d626504b41909a9
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2019-08-23 00:27:03 -0400

    Update notebookTerminal.bash

--- jupyterSetup/notebookTerminal.bash
+++ jupyterSetup/notebookTerminal.bash
@@ -5,6 +5,15 @@ conda activate /home/jovyan/conda/dbconnect
 
 #conda create --name test python=3.5
 #conda init
+###Script to set prebuilt conda env###
+conda config --add envs_dirs /home/jovyan/ISSItutorial/conda
+source /opt/conda/etc/profile.d/conda.sh
+conda activate /home/jovyan/ISSItutorial/conda
+pip install python-igraph --user
+pip show python-igraph
+cp -r ~/.local/lib/python3.7 ~/ISSItutorial/local/lib/python3.7
+
+cp -r ~/ISSItutorial/local/lib/python3.7 ~/.local/lib/python3.7
 
 --------------restart terminal----------------
 
